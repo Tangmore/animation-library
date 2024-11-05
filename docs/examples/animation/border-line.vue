be4d40535a6c109fe159933d8d5ffca1443c28f5
tangli <tangmore@163.com>
2024-11-05 16:16:05 +0800

    按钮

--- docs/examples/animation/border-line.vue
+++ docs/examples/animation/border-line.vue
@@ -1,28 +1,41 @@
 <template>
-  <div class="main">
-    我是按钮
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+  <div class="content">
+    <div class="left-btn">
+      我是按钮
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+
+    <div class="border-ani">
+      button
+    </div>
   </div>
 </template>
 <script setup></script>
+
 <style lang="scss" scoped>
-.main {
+.content {
+  display: flex;
+  align-items: center;
+  gap: 28px;
+}
+
+.left-btn {
   padding: 10px 20px;
   color: rgb(124, 224, 191);
   font-size: 16px;
   border-radius: 5px;
   position: relative;
   overflow: hidden;
+
+  div {
+    position: absolute;
+  }
 }
 
-.main div {
-  position: absolute;
-}
-
-.main :nth-child(1) {
+.left-btn :nth-child(1) {
   top: 0;
   left: 0;
   width: 100%;
@@ -31,7 +44,7 @@
   background: linear-gradient(to right, #fff, #acd, cyan);
 }
 
-.main :nth-child(2) {
+.left-btn :nth-child(2) {
   top: 0;
   right: 0;
   height: 100%;
@@ -40,7 +53,7 @@
   background: linear-gradient(to bottom, #fff, #acd, cyan);
 }
 
-.main :nth-child(3) {
+.left-btn :nth-child(3) {
   bottom: 0;
   left: 0;
   height: 2px;
@@ -49,7 +62,7 @@
   background: linear-gradient(to right, #fff, #acd, cyan);
 }
 
-.main :nth-child(4) {
+.left-btn :nth-child(4) {
   top: 0;
   left: 0;
   height: 100%;
@@ -95,6 +108,45 @@
 
   to {
     transform: translateY(-100%);
+  }
+}
+
+.border-ani {
+  width: 4rem;
+  height: 4rem;
+  border: 1px solid rgb(124, 224, 191);
+  font-size: 16px;
+  border-radius: 50%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: rgb(124, 224, 191);
+
+  &::before {
+    content: '';
+    display: block;
+    width: 4rem;
+    height: 4rem;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 50%;
+    border: 3px solid rgba(124, 224, 191, 0.5);
+    animation: scale 2s linear infinite;
+  }
+}
+
+@keyframes scale {
+  0% {
+    transform: translate(-50%, -50%) scale(1.01);
+    opacity: 1;
+  }
+
+  100% {
+    transform: translate(-50%, -50%) scale(1.5);
+    opacity: 0;
   }
 }
 </style>
